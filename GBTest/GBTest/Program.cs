5c409afcf5b0639dc5c75ef6c8831e7fe0c30761
VicKnyazeva <victoria.knyazeva@hotmail.com>
2022-10-04 22:49:30 +0700

    Task implemented

--- GBTest/GBTest/Program.cs
+++ GBTest/GBTest/Program.cs
@@ -6,7 +6,39 @@ namespace GBTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] input = { "Hello", "2", "world", ":-)" };
+
+            FindStrings(input);
+        }
+
+        private static void FindStrings(string[] input)
+        {
+            int counter = 0;
+
+            foreach (var e in input)
+            {
+                if (e.Length <= 3)
+                    counter++;
+            }
+
+            if (counter == 0)
+                Console.WriteLine("Нужных строк нет");
+            else
+            {
+                string[] output = new string[counter];
+                int index = 0;
+
+                foreach (var e in input)
+                {
+                    if (e.Length <= 3)
+                        output[index++] = e;
+                }
+
+                foreach (var e in output)
+                {
+                    Console.WriteLine(e);
+                }
+            }
         }
     }
 }
